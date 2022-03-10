e8f4f4102602c54b88d3ae2fc14d65097311c163
Ionescu Cristina <58083891+ionescu-cristina@users.noreply.github.com>
2022-03-10 05:36:43 -0800

    add new task

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,41 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div>
+      <h1>All task</h1>
+      <ul>
+        <li v-for="task in tasks" :key="task.id">
+          {{ task.title }}
+        </li>
+      </ul>
+    </div>
+
+    <div>
+      <input type="text" v-model="newTask"/>
+      <button @click="addTask">Add new task</button>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+let id = 0;
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
+  components: {},
+  data() {
+    return {
+      newTask: "",
+      tasks: [
+        { id: id++, title: "Learn Vue", done: true },
+        { id: id++, title: "Learn CSS", done: true },
+        { id: id++, title: "Learn HTML", done: true },
+      ],
+    };
+  },
+  methods: {
+    addTask() {
+      this.tasks.push({id: id++, title: this.newTask, done: false});
+      this.newTask = ''
+    },
   },
 };
 </script>
